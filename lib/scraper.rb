2ee6c4e57a784d37ce05f8c382d14e463c095142
Karen G <kgraves0701@gmail.com>
2018-06-06 14:27:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,26 +6,26 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url) 
     list = Nokogiri::HTML(html)
-    # names = list.css(".student-name")
-    # names_array = []
-    # names.each do |item|
-    #   names_array << item.text
-    # end
-    # names_array
-    #
-    # locations = list.css(".student-location")
-    # location_array = []
-    # locations.each do |item|
-    #   location_array << item.text
-    # end
-    # location_array
-    #
-    # webpages = list.css(".student-card a[href]")
-    # webpage_array = []
-    # webpages.select do |item|
-    # webpage_array << item['href']
-    # end
-    # webpage_array
+    names = list.css(".student-name")
+    names_array = []
+    names.each do |item|
+      names_array << item.text
+    end
+    names_array
+    
+    locations = list.css(".student-location")
+    location_array = []
+    locations.each do |item|
+      location_array << item.text
+    end
+    location_array
+    
+    webpages = list.css(".student-card a[href]")
+    webpage_array = []
+    webpages.select do |item|
+    webpage_array << item['href']
+    end
+    webpage_array
 
   end
 
