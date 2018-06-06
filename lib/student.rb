4d087d8f8474b7593ce393b857ea1c6c24ead2a7
Karen G <kgraves0701@gmail.com>
2018-06-06 14:47:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,9 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array do |student|
-      Scraper.scrape_index_page(student)
-    end
+  
   end
 
   def add_student_attributes(attributes_hash)
